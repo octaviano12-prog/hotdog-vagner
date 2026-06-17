9e2f769050990d1fdcca3f24cf2a30534f60b1a9
octaviano12-prog <octaviano12@gmail.com>
2026-06-17 20:59:23 -0300

    feat: activate customer login requirement for orders

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('./backend/src/routes/premium-patch');
 require('./backend/src/routes/tracking-patch');
 require('./backend/src/routes/customer-auth-patch');
+require('./backend/src/routes/require-customer-order-patch');
 require('./backend/src/server');
