d52c2df452153be52c324ad0b9d049408116583e
octaviano12-prog <octaviano12@gmail.com>
2026-06-17 19:17:30 -0300

    feat: enable customer account backend routes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
 require('./backend/src/routes/premium-patch');
 require('./backend/src/routes/tracking-patch');
+require('./backend/src/routes/customer-auth-patch');
 require('./backend/src/server');
