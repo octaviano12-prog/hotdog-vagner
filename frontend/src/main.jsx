48f2fa9400a132228ef1161414a67a5934055be5
octaviano12-prog <octaviano12@gmail.com>
2026-06-17 16:37:19 -0300

    feat: use premium app entrypoint

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+import PremiumApp from './PremiumApp.jsx';
 import './styles.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <PremiumApp />
   </React.StrictMode>
 );
